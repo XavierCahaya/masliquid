706553e473ab07cab6bc947cd09623b2bdb61a47
Hanss121 <kangjihan121@gmail.com>
2024-12-06 20:12:46 +0700

    finalv.1

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -15,159 +15,130 @@ class LoginPage extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 680,
-            width: 1000,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(166, 236, 255, 1),
-                  Color.fromRGBO(7, 201, 255, 1),
-                  Color.fromRGBO(40, 116, 234, 1),
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 50, bottom: 20),
-                  child: Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('images/LOGO.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Container(
+              height: 680,
+              width: 1000,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(166, 236, 255, 1),
+                    Color.fromRGBO(7, 201, 255, 1),
+                    Color.fromRGBO(40, 116, 234, 1),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
-                Container(
-                  width: 350,
-                  height: 400,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(99, 255, 254, 254),
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/LOGO.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 120,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+                  Container(
+                    width: 350,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(99, 255, 254, 254),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2.0,
                       ),
-                      SizedBox(height: 20),
-                      Container(
-                        width: 280,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'Username',
-                            style: TextStyle(
-                              fontSize: 10,
-                              color: Color.fromARGB(150, 0, 0, 0),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        width: 280,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 10, left: 10),
-                          child: Text(
-                            'Password',
-                            style: TextStyle(
-                              fontSize: 10,
-                              color: Color.fromARGB(150, 0, 0, 0),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, "Home");
-                        },
-                        child: Container(
-                          width: 220,
-                          height: 30,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 120,
+                          height: 50,
                           decoration: BoxDecoration(
-                            color: Colors.blue,
                             borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
                           ),
                           child: Center(
                             child: Text(
-                              'Submit',
+                              'Sign In',
                               style: TextStyle(
-                                fontSize: 12,
+                                fontSize: 25,
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        width: 280,
-                        height: 30,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Don't have an account?",
+                        SizedBox(height: 20),
+                        Container(
+                          width: 280,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 10, left: 10),
+                            child: Text(
+                              'Username',
                               style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black,
+                                fontSize: 10,
+                                color: Color.fromARGB(150, 0, 0, 0),
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(width: 3),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, "SignUp");
-                              },
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          width: 280,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 10, left: 10),
+                            child: Text(
+                              'Password',
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Color.fromARGB(150, 0, 0, 0),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, "Home");
+                          },
+                          child: Container(
+                            width: 220,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                            ),
+                            child: Center(
                               child: Text(
-                                'Sign Up',
+                                'Submit',
                                 style: TextStyle(
                                   fontSize: 12,
                                   color: Colors.white,
@@ -175,51 +146,83 @@ class LoginPage extends State<Login> {
                                 ),
                               ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      Container(
-                        width: 170,
-                        height: 2,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 10),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('images/google.png'),
-                                  fit: BoxFit.contain,
+                        SizedBox(height: 20),
+                        Container(
+                          width: 280,
+                          height: 30,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have an account?",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              height: 20,
-                              width: 30,
-                            ),
-                            SizedBox(width: 2),
-                            Text(
-                              'Continue with google',
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
+                              SizedBox(width: 3),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, "SignUp");
+                                },
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
-                            )
-                          ],
+                            ],
+                          ),
                         ),
-                        width: 200,
-                        height: 30,
-                      ),
-                    ],
+                        SizedBox(height: 10),
+                        Container(
+                          width: 170,
+                          height: 2,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('images/google.png'),
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                height: 20,
+                                width: 30,
+                              ),
+                              SizedBox(width: 2),
+                              Text(
+                                'Continue with google',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                          width: 200,
+                          height: 30,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 import 'pages/login.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -29,30 +30,59 @@ class _SplashScreen extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                height: 680,
-                width: 1000,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromRGBO(166, 236, 255, 1),
-                      Color.fromRGBO(7, 201, 255, 1),
-                      Color.fromRGBO(40, 116, 234, 1),
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage('images/LOGO.png'),
-                  ),
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(166, 236, 255, 1),
+                    Color.fromRGBO(7, 201, 255, 1),
+                    Color.fromRGBO(40, 116, 234, 1),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
               ),
-            ],
-          ),
+            ),
+            Center(
+              child: Container(
+                height: 200,
+                width: 200,
+                child: Center(
+                  child: Image.asset('images/LOGO.png'),
+                ),
+              ),
+            )
+          ],
         ),
+        // body: Center(
+        //   child: Column(
+        //     children: [
+        //       Container(
+        //         height: 680,
+        //         width: 1000,
+        //         decoration: BoxDecoration(
+        //           gradient: LinearGradient(
+        //             colors: [
+        //               Color.fromRGBO(166, 236, 255, 1),
+        //               Color.fromRGBO(7, 201, 255, 1),
+        //               Color.fromRGBO(40, 116, 234, 1),
+        //             ],
+        //             begin: Alignment.topCenter,
+        //             end: Alignment.bottomCenter,
+        //           ),
+
+        //           image: DecorationImage(
+        //             image: AssetImage('images/LOGO.png'),
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
